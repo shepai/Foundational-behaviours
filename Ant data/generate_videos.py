0ede4cffb348b9103744aaa4890cdadd1a5399c4
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2026-09-13 11:17:50 +0100

    infrastructure

---
+++ Ant data/generate_videos.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+
+#given trajectory
+#open up the grid
+#get the image 
+#load into a video
---
+++ Ant data/latent_space_gen.py
@@ -0,0 +1,7 @@
+
+
+#load the video
+#convert to numpy dataset
+#reduce scale of images
+
+#save data somewhere
